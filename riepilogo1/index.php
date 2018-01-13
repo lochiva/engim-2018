4c2848c44ff7eac6e623bcd76f8731f9246e956e
rufus <ruffinengo@lochiva.com>
2018-01-13 15:59:33 +0100

    tolto commenti

--- riepilogo1/index.php
+++ riepilogo1/index.php
@@ -1,18 +1,7 @@
 <?php
 
-// questo è un commento su una riga
-
-// per l'apertura dei tag usiamo sempre la forma estesa: <?php
 
 
-/*
-
- questo è un commento su più righe
-
-*/
-
-
-// questo  è un array
 $pages = [
 	['name'=>'comments'],
 	['name'=>'dichiarazioni'],
